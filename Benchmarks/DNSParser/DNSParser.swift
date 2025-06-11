8dd7b195dd383ff096e33564bbe8790696e74685
MahdiBM <github@mahdibm.com>
2025-06-11 15:29:49 +0330

    more benchmarks

--- Benchmarks/DNSParser/DNSParser.swift
+++ Benchmarks/DNSParser/DNSParser.swift
@@ -8,7 +8,7 @@ let benchmarks: @Sendable () -> Void = {
             metrics: [.cpuUser],
             warmupIterations: 1,
             maxDuration: .seconds(10),
-            maxIterations: 5,
+            maxIterations: 10,
             thresholds: [
                 .cpuUser: .init(
                     /// `3 - 1 == 2`% tolerance.
@@ -32,5 +32,123 @@ let benchmarks: @Sendable () -> Void = {
             let message = try Message(from: &buffer)
             blackHole(message)
         }
+    }
+
+    Benchmark(
+        "Parser_A_Response_Malloc",
+        configuration: .init(
+            metrics: [.mallocCountTotal],
+            warmupIterations: 1,
+            maxDuration: .seconds(10),
+            maxIterations: 10
+        )
+    ) { benchmark in
+        var buffer = Resources.dnsResponseAExampleComPacket.buffer()
+        buffer.moveReaderIndex(forwardBy: 42)
+        buffer.moveDNSPortionStartIndex(forwardBy: 42)
+
+        benchmark.startMeasurement()
+        let message = try Message(from: &buffer)
+        blackHole(message)
+    }
+
+    Benchmark(
+        "100_000xParser_AAAA_Response_CPUUser",
+        configuration: .init(
+            metrics: [.cpuUser],
+            warmupIterations: 1,
+            maxDuration: .seconds(10),
+            maxIterations: 10,
+            thresholds: [
+                .cpuUser: .init(
+                    /// `3 - 1 == 2`% tolerance.
+                    /// Will rely on the absolute threshold as the tighter threshold.
+                    relative: [.p90: 3],
+                    /// 11ms of tolerance.
+                    absolute: [.p90: 11_000_000]
+                )
+            ]
+        )
+    ) { benchmark in
+        var buffer = Resources.dnsResponseAAAACloudflareComPacket.buffer()
+        buffer.moveReaderIndex(forwardBy: 42)
+        buffer.moveDNSPortionStartIndex(forwardBy: 42)
+        let startIndex = buffer.readerIndex
+
+        benchmark.startMeasurement()
+        for _ in 0..<300_000 {
+            buffer.moveReaderIndex(to: startIndex)
+
+            let message = try Message(from: &buffer)
+            blackHole(message)
+        }
+    }
+
+    Benchmark(
+        "Parser_AAAA_Response_Malloc",
+        configuration: .init(
+            metrics: [.mallocCountTotal],
+            warmupIterations: 1,
+            maxDuration: .seconds(10),
+            maxIterations: 10
+        )
+    ) { benchmark in
+        var buffer = Resources.dnsResponseAAAACloudflareComPacket.buffer()
+        buffer.moveReaderIndex(forwardBy: 42)
+        buffer.moveDNSPortionStartIndex(forwardBy: 42)
+
+        benchmark.startMeasurement()
+        let message = try Message(from: &buffer)
+        blackHole(message)
+    }
+
+    Benchmark(
+        "100_000xParser_TXT_Response_CPUUser",
+        configuration: .init(
+            metrics: [.cpuUser],
+            warmupIterations: 1,
+            maxDuration: .seconds(10),
+            maxIterations: 10,
+            thresholds: [
+                .cpuUser: .init(
+                    /// `3 - 1 == 2`% tolerance.
+                    /// Will rely on the absolute threshold as the tighter threshold.
+                    relative: [.p90: 3],
+                    /// 11ms of tolerance.
+                    absolute: [.p90: 11_000_000]
+                )
+            ]
+        )
+    ) { benchmark in
+        var buffer = Resources.dnsResponseTXTExampleComPacket.buffer()
+        buffer.moveReaderIndex(forwardBy: 42)
+        buffer.moveDNSPortionStartIndex(forwardBy: 42)
+        let startIndex = buffer.readerIndex
+
+        benchmark.startMeasurement()
+        for _ in 0..<300_000 {
+            buffer.moveReaderIndex(to: startIndex)
+
+            let message = try Message(from: &buffer)
+            blackHole(message)
+        }
+    }
+
+    Benchmark(
+        "Parser_TXT_Response_Malloc",
+        configuration: .init(
+            metrics: [.mallocCountTotal],
+            warmupIterations: 1,
+            maxDuration: .seconds(10),
+            maxIterations: 10
+        )
+    ) { benchmark in
+        var buffer = Resources.dnsResponseTXTExampleComPacket.buffer()
+        buffer.moveReaderIndex(forwardBy: 42)
+        buffer.moveDNSPortionStartIndex(forwardBy: 42)
+
+        benchmark.startMeasurement()
+        let message = try Message(from: &buffer)
+        blackHole(message)
     }
 }
