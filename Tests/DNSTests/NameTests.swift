7d137a2be57163a5017ecb1c27cf5c8dda3cc54c
MahdiBM <github@mahdibm.com>
2025-07-23 19:47:57 +0330

    Add a Name test against top 200 Cloudflare domains

--- Tests/DNSTests/NameTests.swift
+++ Tests/DNSTests/NameTests.swift
@@ -219,4 +219,38 @@ struct NameTests {
                 == "xn--xkrr14bows.xn--fiqs8s."
         )
     }
+
+    /// The file pointing to `Resources.topDomains` contains only 200 top domains, but you can
+    /// try bigger files too.
+    /// For example you can manually go to cloudflare radar (https://radar.cloudflare.com/domains)
+    /// and download the top 1 million domains csv file (or really top any-number, just csv).
+    /// Just make sure the download file is only 1 column (so only a new domain on each new line).
+    /// Then put it in Tests/Resources/ directory named exactly as `top-domains.csv`.
+    /// And untrack the file so it's not committed to git (it's 14+ MiB).
+    /// The file is 14+ MiB in size so it's not included in the repo.
+    ///
+    /// Not using swift-testing arguments because that slows things down significantly if we're
+    /// testing against 1 million domains.
+    @Test func testAgainstTopCloudflareRadarDomains() throws {
+        for (index, domainName) in enumeratedTopDomains() {
+            let comment: Comment = "index: \(index), domainName: \(domainName)"
+            #expect(throws: Never.self, comment) {
+                let name = try Name(domainName: domainName)
+                let recreatedDomainName = name.description(format: .ascii, options: .sourceAccurate)
+                #expect(recreatedDomainName == domainName, comment)
+            }
+        }
+    }
+}
+
+private func enumeratedTopDomains() -> EnumeratedSequence<[String]> {
+    String(
+        decoding: Resources.topDomains.data(),
+        as: UTF8.self
+    ).split(
+        whereSeparator: \.isNewline
+    )
+    .dropFirst()
+    .map(String.init)
+    .enumerated()
 }
