6a791373fd63e78fd4b37f9d4392831b2e0d146f
Mahdi Bahrami <github@mahdibm.com>
2025-07-30 00:43:27 +0330

    Repair README + Add default EventLoopGroups (#49)

---
+++ Sources/DNSClient/Client/DNSClient+defaultEventLoopGroup.swift
@@ -0,0 +1,31 @@
+public import protocol NIOCore.EventLoopGroup
+import class NIOPosix.MultiThreadedEventLoopGroup
+
+#if canImport(Network)
+import class NIOTransportServices.NIOTSEventLoopGroup
+#endif
+
+@available(swiftDNSApplePlatforms 26, *)
+extension DNSClient {
+    /// Returns the default `EventLoopGroup` singleton for TCP connections, automatically selecting the best for the platform.
+    ///
+    /// This will select the concrete `EventLoopGroup` depending which platform this is running on.
+    public static var defaultTCPEventLoopGroup: any EventLoopGroup {
+        #if canImport(Network)
+        if #available(OSX 10.14, iOS 12.0, tvOS 12.0, watchOS 6.0, *) {
+            return NIOTSEventLoopGroup.singleton
+        } else {
+            return MultiThreadedEventLoopGroup.singleton
+        }
+        #else
+        return MultiThreadedEventLoopGroup.singleton
+        #endif
+    }
+
+    /// Returns the default `EventLoopGroup` singleton for UDP connections, automatically selecting the best for the platform.
+    ///
+    /// This will select the concrete `EventLoopGroup` depending which platform this is running on.
+    public static var defaultUDPEventLoopGroup: any EventLoopGroup {
+        MultiThreadedEventLoopGroup.singleton
+    }
+}
