80a0519f38f73c01165ef8da1f93bc8cabf0c7b2
MahdiBM <github@mahdibm.com>
2025-09-08 22:46:59 +0330

    Move the throughput benchmarks to cpuUser

--- Benchmarks/IPAddress/IPv4AddressToString.swift
+++ Benchmarks/IPAddress/IPv4AddressToString.swift
@@ -3,68 +3,76 @@ import DNSModels
 import NIOCore
 
 let ipv4AddressToStringBenchmarks: @Sendable () -> Void = {
-    // MARK: - IPv4_Zero_String_Description
+    // MARK: - IPv4_String_Encoding_Zero
 
     let ipv4Zero = IPv4Address(0, 0, 0, 0)
     Benchmark(
-        "IPv4_Zero_String_Description",
+        "IPv4_String_Encoding_Zero_10M",
         configuration: .init(
-            metrics: [.throughput],
+            metrics: [.cpuUser],
             warmupIterations: 10,
-            maxIterations: 50_000_000
+            maxIterations: 1000
         )
     ) { benchmark in
-        let description = ipv4Zero.description
-        blackHole(description)
+        for _ in 0..<10_000_000 {
+            let description = ipv4Zero.description
+            blackHole(description)
+        }
     }
 
-    // MARK: - IPv4_Localhost_String_Description
+    // MARK: - IPv4_String_Encoding_Localhost
 
     let ipv4Localhost = IPv4Address(127, 0, 0, 1)
     Benchmark(
-        "IPv4_Localhost_String_Description",
+        "IPv4_String_Encoding_Localhost_10M",
         configuration: .init(
-            metrics: [.throughput],
+            metrics: [.cpuUser],
             warmupIterations: 10,
-            maxIterations: 50_000_000
+            maxIterations: 1000
         )
     ) { benchmark in
-        let description = ipv4Localhost.description
-        blackHole(description)
+        for _ in 0..<10_000_000 {
+            let description = ipv4Localhost.description
+            blackHole(description)
+        }
     }
 
-    // MARK: - IPv4_Local_Broadcast_String_Description
+    // MARK: - IPv4_String_Encoding_Local_Broadcast
 
     let ipv4LocalBroadcast = IPv4Address(255, 255, 255, 255)
     Benchmark(
-        "IPv4_Local_Broadcast_String_Description",
+        "IPv4_String_Encoding_Local_Broadcast_10M",
         configuration: .init(
-            metrics: [.throughput],
+            metrics: [.cpuUser],
             warmupIterations: 10,
-            maxIterations: 50_000_000
+            maxIterations: 1000
         )
     ) { benchmark in
-        let description = ipv4LocalBroadcast.description
-        blackHole(description)
+        for _ in 0..<10_000_000 {
+            let description = ipv4LocalBroadcast.description
+            blackHole(description)
+        }
     }
 
-    // MARK: - IPv4_Mixed_String_Description
+    // MARK: - IPv4_String_Encoding_Mixed
 
     let ipv4Mixed = IPv4Address(123, 45, 6, 0)
     Benchmark(
-        "IPv4_Mixed_String_Description",
+        "IPv4_String_Encoding_Mixed_10M",
         configuration: .init(
-            metrics: [.throughput],
+            metrics: [.cpuUser],
             warmupIterations: 10,
-            maxIterations: 50_000_000
+            maxIterations: 1000
         )
     ) { benchmark in
-        let description = ipv4Mixed.description
-        blackHole(description)
+        for _ in 0..<10_000_000 {
+            let description = ipv4Mixed.description
+            blackHole(description)
+        }
     }
 
     Benchmark(
-        "IPv4_Mixed_String_Description_Malloc",
+        "IPv4_String_Encoding_Mixed_Malloc",
         configuration: .init(
             metrics: [.mallocCountTotal],
             warmupIterations: 1,
