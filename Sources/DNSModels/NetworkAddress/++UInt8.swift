923ee99d561ec2738a58a9bba6735cb4708cdcae
Mahdi Bahrami <github@mahdibm.com>
2025-09-11 12:23:44 +0330

    Better `UIn8(decimalRepresentation:)` init (#70)

--- Sources/DNSModels/NetworkAddress/++UInt8.swift
+++ Sources/DNSModels/NetworkAddress/++UInt8.swift
@@ -2,43 +2,47 @@
 extension UInt8 {
     /// Reads a span of a text like "127" as a `UInt8`, if the bytes are in correct form.
     /// Otherwise returns `nil`.
+    /// Equivalent to `UInt8(string, radix: 10)` but faster.
     @inlinable
-    init?(decimalRepresentation span: Span<UInt8>) {
+    package init?(decimalRepresentation span: Span<UInt8>) {
         let count = span.count
 
-        if count == 0 {
+        guard count > 0, count < 4 else {
             return nil
         }
 
-        self = 0
-
-        /// Unchecked because it must be in range of 1...
+        /// Unchecked because it must be in range of 1...3
         let maxIdx = count &- 1
 
-        for idx in 0..<count {
-            /// Unchecked because both originate from `span.count`
-            let indexInGroup = maxIdx &- idx
-            /// Unchecked because `indexInGroup` is guaranteed to be in range of `0..<count`
-            let utf8Byte = span[unchecked: indexInGroup]
-            guard let decimalDigit = UInt8.mapUTF8ByteToUInt8(utf8Byte) else {
+        guard let first = UInt8.mapUTF8ByteToUInt8(span[unchecked: maxIdx]) else {
+            return nil
+        }
+        self = first
+
+        if count > 1 {
+            guard let second = UInt8.mapUTF8ByteToUInt8(span[unchecked: maxIdx &- 1]) else {
                 return nil
             }
 
-            let factor: UInt8
-            switch idx {
-            case 0: factor = 1
-            case 1: factor = 10
-            case 2: factor = 100
-            default: return nil
+            /// Unchecked because `(self == (0...9)) + (10 * (0...9))` is always in range of `0...99`,
+            /// which is a valid `UInt8`.
+            self &+= 10 &* second
+
+            if count == 3 {
+                /// `count == 3` means `maxIdx == 2`. So instead of
+                /// `span[unchecked: maxIdx &- 2]` we can directly go for `span[unchecked: 0]`.
+                guard let third = UInt8.mapUTF8ByteToUInt8(span[unchecked: 0]) else {
+                    return nil
+                }
+
+                let (value, overflew1) = third.multipliedReportingOverflow(by: 100)
+                if overflew1 { return nil }
+
+                let (newByte, overflew2) = self.addingReportingOverflow(value)
+                if overflew2 { return nil }
+
+                self = newByte
             }
-
-            let (value, overflew1) = decimalDigit.multipliedReportingOverflow(by: factor)
-            if overflew1 { return nil }
-
-            let (newByte, overflew2) = self.addingReportingOverflow(value)
-            if overflew2 { return nil }
-
-            self = newByte
         }
     }
 
