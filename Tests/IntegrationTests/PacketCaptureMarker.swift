bdc9624970cf696e44adf97133bde20182fd2b28
MahdiBM <github@mahdibm.com>
2025-10-12 14:01:22 +0330

    Improve the marker packet in integration tests

--- Tests/IntegrationTests/PacketCaptureMarker.swift
+++ Tests/IntegrationTests/PacketCaptureMarker.swift
@@ -6,20 +6,45 @@ import Testing
 /// A trait that sends a dns message over udp to 240.1.2.3, with the edns part of
 /// the message populated with a message in utf8.
 /// This is so we can see the test boundaries in a packet capture.
-struct PacketCaptureMarkerTrait: TestScoping, TestTrait {
+actor PacketCaptureMarkerTrait: TestScoping, TestTrait {
+    var channel: (any Channel)?
+
+    deinit {
+        try? channel?.close().wait()
+    }
+
+    private init() {}
+
+    static let shared = PacketCaptureMarkerTrait()
+
     func provideScope(
         for test: Test,
         testCase: Test.Case?,
         performing function: @Sendable () async throws -> Void
     ) async throws {
         let testName = test.displayName ?? test.name
-        let word = test.isSuite ? "suite" : "test"
         if #available(swiftDNSApplePlatforms 15, *) {
-            try await self.send(string: "Will run \(word) '\(testName)'")
+            await self.send(
+                subDomain: "test.start",
+                info: "\(testName)"
+            )
         }
-        try await function()
-        if #available(swiftDNSApplePlatforms 15, *) {
-            try await self.send(string: "Finished running \(word) '\(testName)'")
+        do {
+            try await function()
+            if #available(swiftDNSApplePlatforms 15, *) {
+                await self.send(
+                    subDomain: "test.end.with.success",
+                    info: "\(testName)"
+                )
+            }
+        } catch {
+            if #available(swiftDNSApplePlatforms 15, *) {
+                await self.send(
+                    subDomain: "test.end.with.failure",
+                    info: "\(testName)"
+                )
+            }
+            throw error
         }
     }
 
@@ -27,9 +52,29 @@ struct PacketCaptureMarkerTrait: TestScoping, TestTrait {
     /// the message populated with a message in utf8.
     /// This is so we can see the test boundaries in a packet capture.
     @available(swiftDNSApplePlatforms 15, *)
-    func send(string: String) async throws {
+    func send(subDomain: String, info: String) async {
+        do {
+            try await self._send(
+                subDomain: subDomain,
+                info: info
+            )
+        } catch {
+            fatalError(
+                """
+                Failed to send dns marker packet.
+                Error: \(String(reflecting: error))
+                """
+            )
+        }
+    }
+
+    /// Send a dns message over udp to 240.1.2.3, with the edns part of
+    /// the message populated with a message in utf8.
+    /// This is so we can see the test boundaries in a packet capture.
+    @available(swiftDNSApplePlatforms 15, *)
+    func _send(subDomain: String, info: String) async throws {
         var message = try MessageFactory<A>
-            .forQuery(domainName: "running.test.marker.packet.local.")
+            .forQuery(domainName: "\(subDomain).marker.packet.local.")
             .__testing_copyMessage()
         message.header.id = .max
         message.edns = EDNS(
@@ -41,43 +86,46 @@ struct PacketCaptureMarkerTrait: TestScoping, TestTrait {
                 options: [
                     (
                         .unknown(.max),
-                        .unknown(.max, ByteBuffer(string: string))
+                        .unknown(.max, ByteBuffer(string: info))
                     )
                 ]
             )
         )
         var buffer = DNSBuffer()
         try message.encode(into: &buffer)
+        let envelope = AddressedEnvelope(
+            remoteAddress: try! SocketAddress(
+                ipAddress: "240.1.2.3",
+                port: 53
+            ),
+            data: ByteBuffer(dnsBuffer: buffer)
+        )
 
+        let channel = try await self.getChannel()
+        try await channel.writeAndFlush(envelope)
+    }
+
+    func getChannel() async throws -> any Channel {
+        if let channel {
+            return channel
+        }
+        let newChannel = try await self.makeChannel()
+        self.channel = newChannel
+        return newChannel
+    }
+
+    func makeChannel() async throws -> any Channel {
         try await DatagramBootstrap(
             group: NIOSingletons.posixEventLoopGroup
         ).bind(
             host: "0.0.0.0",
             port: 53412
-        ).flatMapThrowing { channel in
-            try NIOAsyncChannel(
-                wrappingChannelSynchronously: channel,
-                configuration: NIOAsyncChannel.Configuration(
-                    inboundType: AddressedEnvelope<ByteBuffer>.self,
-                    outboundType: AddressedEnvelope<ByteBuffer>.self
-                )
-            )
-        }.get().executeThenClose { inbound, outbound in
-            try await outbound.write(
-                AddressedEnvelope(
-                    remoteAddress: try SocketAddress(
-                        ipAddress: "240.1.2.3",
-                        port: 53
-                    ),
-                    data: ByteBuffer(dnsBuffer: buffer)
-                )
-            )
-        }
+        ).get()
     }
 }
 
 extension TestTrait where Self == PacketCaptureMarkerTrait {
     static var packetCaptureMarker: Self {
-        PacketCaptureMarkerTrait()
+        PacketCaptureMarkerTrait.shared
     }
 }
