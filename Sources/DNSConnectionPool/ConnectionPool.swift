3b752ac4d790fb35da17d15f521ad987506a7968
MahdiBM <github@mahdibm.com>
2025-12-19 21:20:59 +0330

    Update postgres-nio conn-pool module

--- Sources/DNSConnectionPool/ConnectionPool.swift
+++ Sources/DNSConnectionPool/ConnectionPool.swift
@@ -116,16 +116,25 @@ public struct ConnectionPoolConfiguration: Sendable {
     /// become idle.
     public var maximumConnectionHardLimit: Int
 
+    /// The amount of time to pass between the first failed connection
+    /// before triggering the circuit breaker.
+    public var circuitBreakerTripAfter: Duration
+
     /// The time that a _preserved_ idle connection stays in the
     /// pool before it is closed.
     public var idleTimeout: Duration
+
+    /// Maximum number of in-progress new connection requests to run at any one time
+    public var maximumConcurrentConnectionRequests: Int
 
     /// initializer
     public init() {
         self.minimumConnectionCount = 0
         self.maximumConnectionSoftLimit = 16
         self.maximumConnectionHardLimit = 16
+        self.circuitBreakerTripAfter = .seconds(60)
         self.idleTimeout = .seconds(60)
+        self.maximumConcurrentConnectionRequests = 20
     }
 }
 
@@ -151,7 +160,7 @@ public final class ConnectionPool<
     public typealias ConnectionFactory = @Sendable (ConnectionID, ConnectionPool<Connection, ConnectionID, ConnectionIDGenerator, Request, RequestID, KeepAliveBehavior, ObservabilityDelegate, Clock>) async throws -> ConnectionAndMetadata<Connection>
 
     @usableFromInline
-    typealias StateMachine = PoolStateMachine<Connection, ConnectionIDGenerator, ConnectionID, Request, Request.ID, CheckedContinuation<Void, Never>>
+    typealias StateMachine = PoolStateMachine<Connection, ConnectionIDGenerator, ConnectionID, Request, Request.ID, CheckedContinuation<Void, Never>, Clock, Clock.Instant>
 
     @usableFromInline
     let factory: ConnectionFactory
@@ -203,7 +212,8 @@ public final class ConnectionPool<
         var stateMachine = StateMachine(
             configuration: .init(configuration, keepAliveBehavior: keepAliveBehavior),
             generator: idGenerator,
-            timerCancellationTokenType: CheckedContinuation<Void, Never>.self
+            timerCancellationTokenType: CheckedContinuation<Void, Never>.self,
+            clock: clock
         )
 
         let (stream, continuation) = AsyncStream.makeStream(of: NewPoolActions.self)
@@ -374,6 +384,15 @@ public final class ConnectionPool<
             self.cancelTimers(timers)
             self.eventContinuation.yield(.makeConnection(request))
 
+        case .makeConnectionsCancelAndScheduleTimers(let requests, let cancelledTimers, let scheduledTimers):
+            self.cancelTimers(cancelledTimers)
+            for request in requests {
+                self.eventContinuation.yield(.makeConnection(request))
+            }
+            for timer in scheduledTimers {
+                self.eventContinuation.yield(.scheduleTimer(timer))
+            }
+
         case .runKeepAlive(let connection, let cancelContinuation):
             cancelContinuation?.resume(returning: ())
             self.eventContinuation.yield(.runKeepAlive(connection))
@@ -390,11 +409,15 @@ public final class ConnectionPool<
             self.closeConnection(connection)
             self.cancelTimers(timers)
 
-        case .shutdown(let cleanup):
+        case .initiateShutdown(let cleanup):
             for connection in cleanup.connections {
                 self.closeConnection(connection)
             }
             self.cancelTimers(cleanup.timersToCancel)
+
+        case .cancelEventStreamAndFinalCleanup(let timersToCancel):
+            self.cancelTimers(timersToCancel)
+            self.eventContinuation.finish()
 
         case .none:
             break
@@ -570,6 +593,8 @@ extension PoolConfiguration {
         self.maximumConnectionHardLimit = configuration.maximumConnectionHardLimit
         self.keepAliveDuration = keepAliveBehavior.keepAliveFrequency
         self.idleTimeoutDuration = configuration.idleTimeout
+        self.circuitBreakerTripAfter = configuration.circuitBreakerTripAfter
+        self.maximumConcurrentConnectionRequests = configuration.maximumConcurrentConnectionRequests
     }
 }
 
@@ -578,13 +603,13 @@ protocol TaskGroupProtocol {
     // We need to call this `addTask_` because some Swift versions define this
     // under exactly this name and others have different attributes. So let's pick
     // a name that doesn't clash anywhere and implement it using the standard `addTask`.
-    mutating func addTask_(operation: @escaping @Sendable () async -> Void)
+    mutating func addTask_(operation: @isolated(any) @escaping @Sendable () async -> Void)
 }
 
 @available(macOS 14.0, iOS 17.0, tvOS 17.0, watchOS 10.0, *)
 extension DiscardingTaskGroup: TaskGroupProtocol {
     @inlinable
-    mutating func addTask_(operation: @escaping @Sendable () async -> Void) {
+    mutating func addTask_(operation: @isolated(any) @escaping @Sendable () async -> Void) {
         self.addTask(priority: nil, operation: operation)
     }
 }
@@ -592,7 +617,7 @@ extension DiscardingTaskGroup: TaskGroupProtocol {
 @available(macOS 10.15, iOS 13, watchOS 6, tvOS 13, *)
 extension TaskGroup<Void>: TaskGroupProtocol {
     @inlinable
-    mutating func addTask_(operation: @escaping @Sendable () async -> Void) {
+    mutating func addTask_(operation: @isolated(any) @escaping @Sendable () async -> Void) {
         self.addTask(priority: nil, operation: operation)
     }
 }
